ffa33fa8dee5a7b15379addac462d986b3ed16d8
Vinícius Resende <vinicius.resende@quattuor.tech>
2020-06-30 20:24:07 -0300

    Upload dos arquivos

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) =>
+    window.getComputedStyle(element).getPropertyValue(style)
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgTimeline: getStyle(html, "--bg-timeline"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text")
+}
+
+const darkMode = {
+    bg: "#222222",
+    bgTimeline: "#343434",
+    colorHeadings: "#f47c03",
+    colorText: "#B5B5B5"
+}
+
+const transformKey = key => "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+const changeColors = (colors) => Object.keys(colors).map(key => html.style.setProperty(transformKey(key), colors[key]))
+
+checkbox.addEventListener("change", ({ target }) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
